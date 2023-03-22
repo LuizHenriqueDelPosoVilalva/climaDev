79af9b2b034c91856f08483589291f8a23697f9d
Luiz Henrique <luizgarciaevilalva@gmail.com>
2023-03-22 11:18:14 -0300

    melhorado index

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,8 +4,13 @@ import BackgroundImageWithChildren from
   '../src/components/layout/backgroundImageWithChildren/BackgroundImageWithChildren'
 
 const StyledDiv = styled.div`
-  background-color: ${(props) => props.theme.black};
+  background-color: ${(props) => props.theme.white};
+  min-width: 500px;
   padding: 10px;
+
+  @media (max-width: 500px){
+    min-width: 100%
+  }
 `
 
 export default function HomePage() {
