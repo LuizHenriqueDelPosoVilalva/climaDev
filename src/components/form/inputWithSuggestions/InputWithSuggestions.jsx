5a85ff7cecebc8499fe7b8dd842fb56fb71a9ae5
Luiz Henrique <luizgarciaevilalva@gmail.com>
2023-03-25 00:37:21 -0300

    criado input com sugestões

---
+++ src/components/form/inputWithSuggestions/InputWithSuggestions.jsx
@@ -0,0 +1,48 @@
+import styled from 'styled-components'
+
+import Input from '../input/Input'
+
+const StyledSuggestion = styled.div`
+  margin-top: 10px;
+  background-color: ${(props) => props.theme.white};
+  box-shadow: 0 4px 4px rgba(0, 0, 0, 0.25);
+`
+
+const Suggestions = styled.div`
+  padding: 10px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #E4E4E4;
+  }
+`
+
+function InputWithSuggestions({ suggestions, ...props }) {
+  return(
+    <>
+      <Input {...props} />
+      <StyledSuggestion>
+        {
+          suggestions.map((suggestion, pos) => 
+          <Suggestions 
+            key={`suggestion-${pos}`}
+            onClick={suggestion.onClick}
+          >
+            {suggestion.text}
+          </Suggestions>
+          )
+        }
+      </StyledSuggestion>
+    </>
+  )
+}
+
+InputWithSuggestions.defaultProps = {
+  suggestions: [
+    { text: 'Sugestão', onclick: () => {} },
+    { text: 'Sugestão 2', onclick: () => {} },
+    { text: 'Sugestão 3', onclick: () => {} }
+  ]
+}
+
+export default InputWithSuggestions
